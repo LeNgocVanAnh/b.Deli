6da9c1ec8c94be559944cd2dff47bd0996148317
LeNgocVanAnh <anhle161108@gmail.com>
2018-10-21 14:14:21 +0700

    update project ver0.2

--- bdeli/Controllers/WebMaster/WebmasterController.cs
+++ bdeli/Controllers/WebMaster/WebmasterController.cs
@@ -33,8 +33,16 @@ namespace bdeli.Controllers.WebMaster
                     Session["Authentication"] = true;
                     return RedirectToAction("Index");
                 }
+                else
+                {
+                    return RedirectToAction("Login", "Webmaster");
+                }
             }
-            return RedirectToAction("Login");
+            else
+            {
+                return RedirectToAction("Login", "Webmaster");
+            }
+            
         }
 
         [HttpGet]
@@ -44,7 +52,11 @@ namespace bdeli.Controllers.WebMaster
             {
                 return RedirectToAction("null");
             }
-            return RedirectToAction("Index");
+            else
+            {
+                return RedirectToAction("Index");
+            }
+            
         }
     }
 }
--- bdeli/Controllers/b.Deli/HomeController.cs
+++ bdeli/Controllers/b.Deli/HomeController.cs
@@ -3,16 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using bdeli.Models;
 
 namespace bdeli.Controllers
 {
     public class HomeController : Controller
     {
+        bdeliEntities db = new bdeliEntities();
         public ActionResult Index()
         {
             return View();
         }
 
-
+        [HttpPost]
+        public ActionResult save(string name, string email, DateTime date, string time, int amount, string note)
+        {
+            var book = new bD_Booking();
+            book.Name = name;
+            book.Email = email;
+            book.DateBooking = date;
+            book.TimeBooking = time;
+            book.Amount = amount;
+            book.Note = note;
+            db.bD_Booking.Add(book);
+            db.Entry(book).State = System.Data.Entity.EntityState.Modified;
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
